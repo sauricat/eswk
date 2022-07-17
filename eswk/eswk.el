bc018bdfb604e4010de2196a6e00744a122ae998
sauricat <linshu1729@protonmail.com>
2022-07-18 01:04:09 +0800

    eswk: init template

---
+++ eswk/eswk.el
@@ -0,0 +1,40 @@
+;;; eswk.el --- Emacs Sway Knitter  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2015-2021 Free Software Foundation, Inc.
+
+;; Author: Shu Lin <linshu1729@protonmail.com>
+;; Version: 0.1
+;; Package-Requires:
+;; Keywords: unix
+;; URL: https://github.com/sauricat/eswk
+
+;; This file is not part of GNU Emacs.
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later
+;; version.
+;;
+;; This is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(defgroup eswk nil
+  "EMACS Sway Knitter."
+  :tag "ESWK"
+  :version "29.0"
+  :group 'applications
+  :prefix "eswk-")
+
+(provide 'eswk)
+;;; eswk.el ends here
