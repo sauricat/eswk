15e0e0663e04e9e2e43252b0eb129db7668697c9
sauricat <linshu1729@protonmail.com>
2022-07-18 06:25:41 +0800

    flake: add test vm environment

--- flake.nix
+++ flake.nix
@@ -14,9 +14,18 @@ exec nix repl $testnix
       inputs.flake-utils.follows = "flake-utils";
     };
   };
+
   outputs = { nixpkgs, flake-utils, emacs-overlay, self, ... }: let
-    emacsSystems = builtins.attrNames emacs-overlay.packages;
-  in flake-utils.lib.eachSystem emacsSystems (system: rec {
+    emacsSystems = {
+      x64 = "x86_64-linux";
+      x86 = "i686-linux";
+      arm = "aarch64-linux";
+    };
+    emacsSystemList = builtins.attrValues emacsSystems;
+    #   nixpkgs.lib.lists.intersectLists
+    #     nixpkgs.legacyPackages.x86_64-linux.sway.meta.platforms
+    #     (builtins.attrNames emacs-overlay.packages);
+  in flake-utils.lib.eachSystem emacsSystemList (system: rec {
     packages = {
       eswk = nixpkgs.legacyPackages.${system}.emacsPackages.trivialBuild {
         pname = "eswk";
@@ -32,5 +41,48 @@ exec nix repl $testnix
     in {
       # TODO
     };
+
+    # Run `nixos-rebuild build-vm --no-build-nix --flake path:.#eswk-test-vm-<arch>` to build a test env.
+    nixosConfigurations = let
+      lib = nixpkgs.lib;
+      vm = system: lib.makeOverridable lib.nixosSystem {
+        inherit system;
+        specialArgs = { };
+        modules = [ self.nixosModules.eswk ] ++ lib.singleton ({ pkgs, ... }: let
+          emacsTestEnv = pkgs.emacsWithPackagesFromUsePackage {
+            config = ./test/init.el;
+            package = pkgs.emacsPgtk;
+            extraEmacsPackages = epkgs: [ self.packages.${system}.eswk ];
+          };
+        in {
+          imports = [
+            (nixpkgs + /nixos/modules/virtualisation/qemu-vm.nix) # Why?
+          ];
+          nixpkgs.overlays = [ emacs-overlay.overlay ];
+          environment.systemPackages = with pkgs; [
+            emacsTestEnv
+            sway # Don't use `config.packages.sway`.
+            foot
+          ];
+          environment.variables = {
+            WLR_NO_HARDWARE_CURSORS = "1";
+          };
+          users.users.tester = {
+            isNormalUser = true;
+            password = "1234";
+            extraGroups = [ "wheel" ];
+          };
+          security.sudo = {
+            enable = true;
+            wheelNeedsPassword = false;
+          };
+          virtualisation = {
+            diskSize = 5120;
+            qemu.options = [ "-vga none" "-device virtio-gpu-pci" ]; # sway can't run in a virtualized GPU.
+          };
+          system.stateVersion = "22.11";
+        });
+      };
+    in lib.mapAttrs' (name: system: lib.nameValuePair ("eswk-test-vm-" + name) (vm system)) emacsSystems;
   };
 }
